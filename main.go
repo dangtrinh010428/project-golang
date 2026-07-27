ca2974e50bb3e7182222d0287049eb0d563073a7
Đăng Trình <dtrinhit04@gmail.com>
2026-07-27 08:09:53 +0700

    handler update architecture

--- main.go
+++ main.go
@@ -79,31 +79,7 @@ func GetAllItem(db *gorm.DB) func(*gin.Context) {
 
 func Update(db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
-		var data model.TodoItemUpdate
-		id, err := strconv.Atoi(c.Param("id"))
-
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		if err := c.ShouldBind(&data); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		if err := db.Where("id = ?", id).Updates(&data).Error; err != nil {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, common.SimpleSuccessResponse(true))
+		
 	}
 }
 
---
+++ module/item/transport/gin/update_Item_handler.go
@@ -0,0 +1,47 @@
+package ginitem
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"todo.com/mod/common"
+	"todo.com/mod/module/item/biz"
+	"todo.com/mod/module/item/model"
+	"todo.com/mod/module/item/storage"
+)
+
+// Tạo mới item
+func UpdateItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data model.TodoItemUpdate
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		store := storage.NewSQLStore(db)
+		business := biz.NewUpdateItemBiz(store)
+
+		if err := business.UpdateItemItemById(c.Request.Context(), id, &data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(true))
+	}
+}
